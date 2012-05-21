c0f3df3aa0857c2a6775923f9db81724765e9198
John Griffith <john.griffith@solidfire.com>
2012-05-21 12:27:13 -0600

    just need an api to talk to cinder service, and fake for testing purposes

---
+++ nova/volume/cinder/api.py
@@ -0,0 +1,2 @@
+#All nova calls related to volumes using the cinder volume-service will go through this api
+#this should be all we need, everything else will be handled in the cinder service itself.

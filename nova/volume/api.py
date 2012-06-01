33af47851dab2f5c32e435f861b50d8aec3bbdef
Anthony Young <sleepsonthefloor@gmail.com>
2012-06-01 12:58:06 -0700

    Working on cinder integration
    Change-Id: Ie600b0f0644349a20a9b742c7d51008de6347b54

--- nova/volume/api.py
+++ nova/volume/api.py
@@ -63,7 +63,7 @@ def check_policy(context, action, target_obj=None):
     nova.policy.enforce(context, _action, target)
 
 
-class API(base.Base):
+class _API(base.Base):
     """API for interacting with the volume manager."""
 
     def create(self, context, size, name, description, snapshot=None,
@@ -326,6 +326,332 @@ class API(base.Base):
     @wrap_check_policy
     def delete_volume_metadata(self, context, volume, key):
         """Delete the given metadata item from a volume."""
+        self.db.volume_metadata_delete(context, volume['id'], key)
+
+    @wrap_check_policy
+    def update_volume_metadata(self, context, volume, metadata, delete=False):
+        """Updates or creates volume metadata.
+
+        If delete is True, metadata items that are not specified in the
+        `metadata` argument will be deleted.
+
+        """
+        if delete:
+            _metadata = metadata
+        else:
+            _metadata = self.get_volume_metadata(context, volume['id'])
+            _metadata.update(metadata)
+
+        self.db.volume_metadata_update(context, volume['id'], _metadata, True)
+        return _metadata
+
+    def get_volume_metadata_value(self, volume, key):
+        """Get value of particular metadata key."""
+        metadata = volume.get('volume_metadata')
+        if metadata:
+            for i in volume['volume_metadata']:
+                if i['key'] == key:
+                    return i['value']
+        return None
+
+
+def cinderclient(context):
+    LOG.debug('cinderclient connection created using token "%s" and url "%s"' %
+              (request.user.token, url_for(request, 'volume')))
+
+    # FIXME!!!  Get the proper url from keystone!!!
+    url = "http://127.0.0.1:8776/v1/"
+    c = nova_client.Client(context.auth_user,
+                           context.auth_token,
+                           project_id=context.project_id,
+                           auth_url=url)
+    c.client.auth_token = context.auth_token
+    c.client.management_url = url
+    return c
+
+
+def _untranslate_volume_summary_view(context, vol):
+    """Maps keys for volumes summary view."""
+    d = {}
+
+    d['id'] = vol['id']
+    d['status'] = vol['status']
+    d['size'] = vol['size']
+    d['availability_zone'] = vol['availabilityZone']
+    d['created_at'] = vol['createdAt']
+
+    if vol['attachments'][0]:
+        att = vol['attachments'][0]
+        d['attach_status'] = 'attached'
+        d['instance_uuid'] = att['serverId']
+        d['mountpoint'] = d.get('device')
+    else:
+         d['attach_status'] = 'detached'
+
+    d['display_name'] = vol['displayName']
+    d['display_description'] = vol['displayDescription']
+    
+    # FIXME: Information may be lost in this translation
+    d['volume_type_id'] = vol['volumeType']
+    d['snapshot_id'] = vol['snapshotId']
+    LOG.audit(_("vol=%s"), vol, context=context)
+
+    d['vol_metadata'] = []
+    for k, v in vol['metadata']:
+        item = {}
+        item['key'] = k
+        item['value'] = v
+        d['vol_metadata'].append(item)
+
+    return d
+
+
+class API(base.Base):
+    """API for interacting with the volume manager."""
+
+    def create(self, context, size, name, description, snapshot=None,
+                     volume_type=None, metadata=None, availability_zone=None):
+        check_policy(context, 'create')
+        if snapshot is not None:
+            if snapshot['status'] != "available":
+                msg = _("status must be available")
+                raise exception.InvalidSnapshot(reason=msg)
+            if not size:
+                size = snapshot['volume_size']
+
+            snapshot_id = snapshot['id']
+        else:
+            snapshot_id = None
+
+        if quota.allowed_volumes(context, 1, size) < 1:
+            pid = context.project_id
+            LOG.warn(_("Quota exceeded for %(pid)s, tried to create"
+                    " %(size)sG volume") % locals())
+            raise exception.VolumeSizeTooLarge()
+
+        if availability_zone is None:
+            availability_zone = FLAGS.storage_availability_zone
+
+        if volume_type is None:
+            volume_type_id = None
+        else:
+            volume_type_id = volume_type.get('id', None)
+
+        options = {
+            'size': size,
+            'user_id': context.user_id,
+            'project_id': context.project_id,
+            'snapshot_id': snapshot_id,
+            'availability_zone': availability_zone,
+            'status': "creating",
+            'attach_status': "detached",
+            'display_name': name,
+            'display_description': description,
+            'volume_type_id': volume_type_id,
+            'metadata': metadata,
+            }
+
+        volume = self.db.volume_create(context, options)
+        rpc.cast(context,
+                 FLAGS.scheduler_topic,
+                 {"method": "create_volume",
+                  "args": {"topic": FLAGS.volume_topic,
+                           "volume_id": volume['id'],
+                           "snapshot_id": snapshot_id}})
+        return volume
+
+    # TODO(yamahata): eliminate dumb polling
+    def wait_creation(self, context, volume):
+        volume_id = volume['id']
+        while True:
+            volume = self.get(context, volume_id)
+            if volume['status'] != 'creating':
+                return
+            greenthread.sleep(1)
+
+    @wrap_check_policy
+    def delete(self, context, volume):
+        volume_id = volume['id']
+        if not volume['host']:
+            # NOTE(vish): scheduling failed, so delete it
+            self.db.volume_destroy(context, volume_id)
+            return
+        if volume['status'] not in ["available", "error"]:
+            msg = _("Volume status must be available or error")
+            raise exception.InvalidVolume(reason=msg)
+
+        snapshots = self.db.snapshot_get_all_for_volume(context, volume_id)
+        if len(snapshots):
+            msg = _("Volume still has %d dependent snapshots") % len(snapshots)
+            raise exception.InvalidVolume(reason=msg)
+
+        now = utils.utcnow()
+        self.db.volume_update(context, volume_id, {'status': 'deleting',
+                                                   'terminated_at': now})
+        host = volume['host']
+        rpc.cast(context,
+                 self.db.queue_get_for(context, FLAGS.volume_topic, host),
+                 {"method": "delete_volume",
+                  "args": {"volume_id": volume_id}})
+
+    @wrap_check_policy
+    def update(self, context, volume, fields):
+        self.db.volume_update(context, volume['id'], fields)
+
+    def get(self, context, volume_id):
+        item = cinderclient(context).volumes.get(volume_id)
+        return _untranslate_volume_summary_view(context, item)
+
+    def get_all(self, context, search_opts={}):
+        check_policy(context, 'get_all')
+        items = cinderclient(context).volumes.list(detailed=True)
+        rval = []
+
+        for item in items:
+            rval.append(_untranslate_volume_summary_view(context, item))
+
+        return rval
+
+    def get_snapshot(self, context, snapshot_id):
+        check_policy(context, 'get_snapshot')
+        rv = self.db.snapshot_get(context, snapshot_id)
+        return dict(rv.iteritems())
+
+    def get_all_snapshots(self, context):
+        check_policy(context, 'get_all_snapshots')
+        if context.is_admin:
+            return self.db.snapshot_get_all(context)
+        return self.db.snapshot_get_all_by_project(context, context.project_id)
+
+    @wrap_check_policy
+    def check_attach(self, context, volume):
+        # TODO(vish): abstract status checking?
+        if volume['status'] != "available":
+            msg = _("status must be available")
+            raise exception.InvalidVolume(reason=msg)
+        if volume['attach_status'] == "attached":
+            msg = _("already attached")
+            raise exception.InvalidVolume(reason=msg)
+
+    @wrap_check_policy
+    def check_detach(self, context, volume):
+        # TODO(vish): abstract status checking?
+        if volume['status'] == "available":
+            msg = _("already detached")
+            raise exception.InvalidVolume(reason=msg)
+
+    def remove_from_compute(self, context, volume, instance_id, host):
+        """Remove volume from specified compute host."""
+        rpc.call(context,
+                 self.db.queue_get_for(context, FLAGS.compute_topic, host),
+                 {"method": "remove_volume_connection",
+                  "args": {'instance_id': instance_id,
+                           'volume_id': volume['id']}})
+
+    @wrap_check_policy
+    def reserve_volume(self, context, volume):
+        self.update(context, volume, {"status": "attaching"})
+
+    @wrap_check_policy
+    def unreserve_volume(self, context, volume):
+        if volume['status'] == "attaching":
+            self.update(context, volume, {"status": "available"})
+
+    @wrap_check_policy
+    def attach(self, context, volume, instance_uuid, mountpoint):
+        host = volume['host']
+        queue = self.db.queue_get_for(context, FLAGS.volume_topic, host)
+        return rpc.call(context, queue,
+                        {"method": "attach_volume",
+                         "args": {"volume_id": volume['id'],
+                                  "instance_uuid": instance_uuid,
+                                  "mountpoint": mountpoint}})
+
+    @wrap_check_policy
+    def detach(self, context, volume):
+        host = volume['host']
+        queue = self.db.queue_get_for(context, FLAGS.volume_topic, host)
+        return rpc.call(context, queue,
+                 {"method": "detach_volume",
+                  "args": {"volume_id": volume['id']}})
+
+    @wrap_check_policy
+    def initialize_connection(self, context, volume, connector):
+        host = volume['host']
+        queue = self.db.queue_get_for(context, FLAGS.volume_topic, host)
+        return rpc.call(context, queue,
+                        {"method": "initialize_connection",
+                         "args": {"volume_id": volume['id'],
+                                  "connector": connector}})
+
+    @wrap_check_policy
+    def terminate_connection(self, context, volume, connector):
+        self.unreserve_volume(context, volume)
+        host = volume['host']
+        queue = self.db.queue_get_for(context, FLAGS.volume_topic, host)
+        return rpc.call(context, queue,
+                        {"method": "terminate_connection",
+                         "args": {"volume_id": volume['id'],
+                                  "connector": connector}})
+
+    def _create_snapshot(self, context, volume, name, description,
+                         force=False):
+        check_policy(context, 'create_snapshot', volume)
+
+        if ((not force) and (volume['status'] != "available")):
+            msg = _("must be available")
+            raise exception.InvalidVolume(reason=msg)
+
+        options = {
+            'volume_id': volume['id'],
+            'user_id': context.user_id,
+            'project_id': context.project_id,
+            'status': "creating",
+            'progress': '0%',
+            'volume_size': volume['size'],
+            'display_name': name,
+            'display_description': description}
+
+        snapshot = self.db.snapshot_create(context, options)
+        host = volume['host']
+        rpc.cast(context,
+                 self.db.queue_get_for(context, FLAGS.volume_topic, host),
+                 {"method": "create_snapshot",
+                  "args": {"volume_id": volume['id'],
+                           "snapshot_id": snapshot['id']}})
+        return snapshot
+
+    def create_snapshot(self, context, volume, name, description):
+        return self._create_snapshot(context, volume, name, description,
+                                     False)
+
+    def create_snapshot_force(self, context, volume, name, description):
+        return self._create_snapshot(context, volume, name, description,
+                                     True)
+
+    @wrap_check_policy
+    def delete_snapshot(self, context, snapshot):
+        if snapshot['status'] not in ["available", "error"]:
+            msg = _("Volume Snapshot status must be available or error")
+            raise exception.InvalidVolume(reason=msg)
+        self.db.snapshot_update(context, snapshot['id'],
+                                {'status': 'deleting'})
+        volume = self.db.volume_get(context, snapshot['volume_id'])
+        host = volume['host']
+        rpc.cast(context,
+                 self.db.queue_get_for(context, FLAGS.volume_topic, host),
+                 {"method": "delete_snapshot",
+                  "args": {"snapshot_id": snapshot['id']}})
+
+    @wrap_check_policy
+    def get_volume_metadata(self, context, volume):
+        """Get all metadata associated with a volume."""
+        rv = self.db.volume_metadata_get(context, volume['id'])
+        return dict(rv.iteritems())
+
+    @wrap_check_policy
+    def delete_volume_metadata(self, context, volume, key):
+        """Delete the given metadata item from an volume."""
         self.db.volume_metadata_delete(context, volume['id'], key)
 
     @wrap_check_policy
